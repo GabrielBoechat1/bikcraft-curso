83fae411da2e49e743bedc4b9d93c55c52aa9764
Gabriel <Gabriel Barbosa>
2022-11-12 17:47:05 -0300

    Fazendo um 'Menu Ativo' usando JavaScript

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+    const url = location.href;
+    if (url.includes(link.href)) {
+        link.classList.add('ativo');
+    };
+};
+
+links.forEach(ativarLink);
